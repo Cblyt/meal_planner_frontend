0f51e4006425dcdea200b3737ebc2faba63d572d
Cblyt <connor_blyth@outlook.com>
2021-05-10 10:41:20 +0100

    Improvement to shopping list

--- src/App.jsx
+++ src/App.jsx
@@ -5,51 +5,89 @@ import RecipePage from './containers/RecipePage/RecipePage';
 import BrowseRecipesPage from './containers/BrowseRecipesPage/BrowseRecipesPage';
 import CreateRecipePage from './containers/CreateRecipePage/CreateRecipePage';
 import CheapestProductListPage from './containers/CheapestProductListPage/CheapestProductListPage';
-import Home from './containers/HomePage/HomePage';
 import SidebarShoppingList from './components/SidebarShoppingList/SidebarShoppingList';
 import APIRequest from './components/Helpers/APIHandler/APIHandler';
+import CheckoutPage from './containers/CheckoutPage/CheckoutPage';
 
 const App = () => {
 	const [shoppingList, setShoppingList] = React.useState([]);
-	const [id, setId] = React.useState('');
 
-	const addRecipeToShoppingList = (newId) => {
-		setId(newId);
+	const sortShoppingList = (toSort) => {
+		const o = {};
+
+		toSort.forEach((item) => {
+			const tempId = item.id;
+			if (!o[tempId]) {
+				o[tempId] = item;
+			} else {
+				o[tempId].amount += item.amount;
+			}
+		});
+		const a2 = [];
+		Object.keys(o).forEach((key) => {
+			a2.push(o[key]);
+		});
+		setShoppingList(a2);
 	};
 
-	React.useEffect(async () => {
+	const addRecipeToShoppingList = async (id) => {
 		const response = await APIRequest('get', `/api/recipes/${id}`);
 		if (response.data.ingredients) {
 			const oldShoppingList = [...shoppingList];
-			setShoppingList([...oldShoppingList, ...response.data.ingredients]);
+			sortShoppingList([...oldShoppingList, ...response.data.ingredients]);
 		}
-	}, [id]);
+	};
+
+	const deleteShoppingItemHandler = (ingredientId) => {
+		const oldShoppingList = [...shoppingList];
+		oldShoppingList.splice(ingredientId, 1);
+		setShoppingList(oldShoppingList);
+	};
 
 	return (
 		<div>
 			<Router>
 				<Layout>
-
-					<div>
-						<Switch>
-							<Route path="/recipe/:id">
-								<RecipePage addRecipeToShoppingList={addRecipeToShoppingList} />
-							</Route>
-							<Route path="/create-recipe">
-								<CreateRecipePage />
-							</Route>
-							<Route path="/cheapest-product-list/:id">
-								<CheapestProductListPage />
-							</Route>
-							<Route path="/recipes">
-								<BrowseRecipesPage />
-							</Route>
-							<Route path="/">
-								<Home />
-							</Route>
-						</Switch>
-					</div>
-					<SidebarShoppingList shoppingList={shoppingList} />
+					<Switch>
+						<Route path="/recipe/:id">
+							<RecipePage addRecipeToShoppingList={addRecipeToShoppingList} />
+							<SidebarShoppingList
+								shoppingList={shoppingList}
+								deleteShoppingItem={deleteShoppingItemHandler}
+							/>
+						</Route>
+						<Route path="/create-recipe">
+							<CreateRecipePage />
+						</Route>
+						<Route path="/cheapest-product-list/:id">
+							<CheapestProductListPage />
+							<SidebarShoppingList
+								shoppingList={shoppingList}
+								deleteShoppingItem={deleteShoppingItemHandler}
+							/>
+						</Route>
+						<Route path="/recipes">
+							<BrowseRecipesPage />
+							<SidebarShoppingList
+								shoppingList={shoppingList}
+								deleteShoppingItem={deleteShoppingItemHandler}
+							/>
+						</Route>
+						<Route path="/checkout">
+							<CheckoutPage shoppingList={shoppingList} />
+							<SidebarShoppingList
+								shoppingList={shoppingList}
+								deleteShoppingItem={deleteShoppingItemHandler}
+							/>
+						</Route>
+						<Route path="/">
+							<BrowseRecipesPage />
+							<SidebarShoppingList
+								shoppingList={shoppingList}
+								deleteShoppingItem={deleteShoppingItemHandler}
+							/>
+						</Route>
+					</Switch>
 				</Layout>
 			</Router>
 		</div>
